eadb1597bdf1f652cef12ce0a4e4365096eafd46
Damien Garaud <damien.garaud-ext@sgcib.com>
2014-12-12 14:33:57 +0100

    ezbbg: add the historical data getter to the client

--- client.py
+++ client.py
@@ -3,11 +3,15 @@
 import json
 
 import requests
+import pandas as pd
+
+__author__ = ('eruiz070210', 'dgaraud111714')
 
 HOST = 'localhost'
 PORT = 5555
 HEADERS = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 URL_REFERENCE_DATA = "http://{0}:{1}/reference_data"
+URL_HISTORICAL_DATA = "http://{0}:{1}/historical_data"
 
 def get_reference_data(ticker_list, field_list, host=HOST, port=PORT, **kwargs):
     reference_data_request = {
@@ -22,8 +26,29 @@ def get_reference_data(ticker_list, field_list, host=HOST, port=PORT, **kwargs):
     response.raise_for_status()
     return response.json()
 
+def get_historical_data(ticker_list, field_list, start_date, end_date,
+                        host=HOST, port=PORT, **kwargs):
+    historical_data_request = {
+        'ticker_list': ticker_list,
+        'field_list': field_list,
+        'start_date': start_date.isoformat(),
+        'end_date': end_date.isoformat()}
+    historical_data_request.update(kwargs)
+    historical_data_request_js = json.dumps(historical_data_request)
+    response = requests.get(URL_HISTORICAL_DATA.format(host, port),
+                            data=historical_data_request_js,
+                            headers=HEADERS)
+    response.raise_for_status()
+    data_dict_json = response.json()
+    return {k: pd.read_json(v) for k,v in data_dict_json.iteritems()}
+
+
 if __name__ == "__main__":
+    from datetime import date
     host = 'FR09256841D'
+    #host = 'localhost'
     ticker_list = ['SX5E Index', 'SPX Index']
-    field_list = ['PX_LAST']
+    field_list = ['PX_OPEN', 'PX_LAST']
     data = get_reference_data(ticker_list, field_list, host)
+    start, end = date(2012, 1, 1), date(2014, 1, 1)
+    histo_data = get_historical_data(ticker_list, field_list, start, end, host)
