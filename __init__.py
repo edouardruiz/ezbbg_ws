321a2e331f2572dca4186792377575417b372678
Damien Garaud <damien.garaud-ext@sgcib.com>
2015-05-20 17:30:57 +0200

    New function 'git_version' to get the latest Web Service version

--- __init__.py
+++ __init__.py
@@ -5,4 +5,17 @@
 Run the server side with `python -m ezbbg.ws`
 """
 
+import os.path as osp
+
+
 __author__ = 'dgaraud111714'
+
+
+def git_version():
+    """Get the Web Service 'version'
+
+    i.e. a representation of the latest Git commit.
+    """
+    wsdir = osp.dirname(__file__)
+    with open(osp.join(wsdir, "version")) as fobj:
+        return fobj.read().strip()
