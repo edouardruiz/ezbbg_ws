dde43257603976898225967eec60454b6f956598
Damien Garaud <damien.garaud-ext@sgcib.com>
2015-05-21 09:36:40 +0200

    Allow to use the env. variable EZBBG_SERVER_DEBUG to launch the server

--- __main__.py
+++ __main__.py
@@ -2,7 +2,10 @@
 
 __author__ = "dgaraud111714"
 
+import os
+
 from .server import main
 
 if __name__ == '__main__':
-    main()
+    debug = os.environ.get("EZBBG_SERVER_DEBUG", False)
+    main(debug)
